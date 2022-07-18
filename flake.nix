a34d47d3997546a8633e38d944c9b425a7aed3de
Joshua McQuistan <joshua.mcquistan@feral.io>
2022-07-18 15:56:57 +0100

    Update to Go 1.18

--- flake.nix
+++ flake.nix
@@ -1,11 +1,21 @@
 {
-  description = "handles incoming sflow packets";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11-small";
+  description = "protoc Elm plugin";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05-small";
 
   outputs = { self, nixpkgs }:
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
       devShell.x86_64-linux = with pkgs;
-        mkShell { nativeBuildInputs = [ go protobuf ]; };
+        mkShell {
+          nativeBuildInputs = [
+            go_1_18
+            go-protobuf
+            elmPackages.elm
+            elmPackages.elm-format
+            elmPackages.elm-live
+            elmPackages.elm-test
+            protobuf
+          ];
+        };
     };
 }
